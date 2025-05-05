6a8cf689cc027a32b68b87b80cd1244f8e2240ba
liamgines <109954426+liamgines@users.noreply.github.com>
2025-05-04 22:34:58 -0700

    Use how long the device has been running to calculate kilowatt hours and show this to the client

--- echo_server.py
+++ echo_server.py
@@ -29,6 +29,18 @@ def amps_to_kilowatts(amps, volts=120):
     kilowatts = amps * volts / 1000
     return kilowatts
 
+def amps_to_kilowatt_hours(amps, hours, volts=120):
+    kilowatts = amps_to_kilowatts(amps, volts)
+    kilowatt_hours = kilowatts * hours
+    return kilowatt_hours
+
+def seconds_to_hours(seconds):
+    MINUTE_PER_SECONDS = 1 / 60
+    HOUR_PER_MINUTES = 1 / 60
+    HOURS_PER_SECOND = HOUR_PER_MINUTES * MINUTE_PER_SECONDS
+    hours = seconds * HOURS_PER_SECOND
+    return hours
+
 def get_client_requested_data(query_index):
     # Fetches data from Neon database
     cursor.execute('select PAYLOAD, TIME from "Assignment #8 Destination Table_virtual"')
@@ -75,16 +87,28 @@ def get_client_requested_data(query_index):
 
     elif query_index == 2:
         electricity_consumption_by_device_id = {FIRST_FRIDGE_ID : 0, DISHWASHER_ID : 0, SECOND_FRIDGE_ID : 0}
+        time_running_by_device_id = {FIRST_FRIDGE_ID : 0, DISHWASHER_ID : 0, SECOND_FRIDGE_ID : 0}
         for row in selected_rows:
             current_payload = row[0]
+            current_creation_time = row[1]
             current_device_id = current_payload["parent_asset_uid"]
+
+            time_diff = datetime.now(timezone.utc) - current_creation_time
             
             if current_device_id == FIRST_FRIDGE_ID:
                 electricity_consumption_by_device_id[FIRST_FRIDGE_ID] += float(current_payload["ACS712 - ACS712 - Ammeter (Fridge)"])
+                time_running_by_device_id[FIRST_FRIDGE_ID] = max(time_running_by_device_id[FIRST_FRIDGE_ID], seconds_to_hours(time_diff.seconds))
+
             elif current_device_id == DISHWASHER_ID:
                 electricity_consumption_by_device_id[DISHWASHER_ID] += float(current_payload["ACS712 - Ammeter (Dishwasher)"])
+                time_running_by_device_id[DISHWASHER_ID] = max(time_running_by_device_id[DISHWASHER_ID], seconds_to_hours(time_diff.seconds))
+
             elif current_device_id == SECOND_FRIDGE_ID:
                 electricity_consumption_by_device_id[SECOND_FRIDGE_ID] += float(current_payload["sensor 1 27a451a2-eac4-471d-8cf7-de13d8900eaf"])
+                time_running_by_device_id[SECOND_FRIDGE_ID] = max(time_running_by_device_id[SECOND_FRIDGE_ID], seconds_to_hours(time_diff.seconds))
+
+        for device_id in electricity_consumption_by_device_id:
+            electricity_consumption_by_device_id[device_id] = amps_to_kilowatt_hours(electricity_consumption_by_device_id[device_id], time_running_by_device_id[device_id])
 
         most_power_hungry_device_id = False
         most_electricity_consumed = 0
@@ -96,7 +120,7 @@ def get_client_requested_data(query_index):
         for device_id in electricity_consumption_by_device_id:
             current_electricity_consumed = electricity_consumption_by_device_id[device_id]
             if current_electricity_consumed == most_electricity_consumed:
-                client_requested_data += f"{device_name_lookup[device_id]} consumed the most electricity among all devices at {amps_to_kilowatts(most_electricity_consumed):.2f} kilowatts\n"
+                client_requested_data += f"{device_name_lookup[device_id]} consumed the most electricity among all devices at {most_electricity_consumed:.2f} kilowatt hours\n"
 
     else:
         raise ValueError
